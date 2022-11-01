07ddb0f056e61eec25fa651785e60bbb9efc1e5f
ivasdtmbb <ivasdtmbb@gmail.com>
2022-11-01 18:44:43 +0600

    Add io/hexlet/One.java

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,8 +1,11 @@
 package io.hexlet;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class One {
     public static void main(String[] args) {
-	Two.greeting();
+	//	Two.greeting();
+	System.out.println(StringUtils.reverse("Hexlet"));
     }
 }
 
