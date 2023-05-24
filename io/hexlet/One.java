ea4bc6edff598eeddd0d2ef465e0fd179d60bbf3
ivasdtmbb <ivasdtmbb@gmail.com>
2023-05-24 17:21:45 +1000

    Modify One.java

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,9 +1,11 @@
 package io.hexlet;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class One {
     public static void main(String[] args) {
-	Two.greeting();
-	//System.out.println(StringUtils.reverse("Hexlet"));
+	//	Two.greeting();
+	System.out.println(StringUtils.reverse("Hexlet"));
     }
 }
 
